559fe41fdb061eee26070b95898ec2e717d2f172
bhumee2009 <84014837+bhumee2009@users.noreply.github.com>
2021-08-17 16:48:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var mouseEvent="empty";
+var last_position_x, last_position_y;
+
+canvas=document.getElementById("myCanvas").value;
+ctx=canvas.getContext("2d");
+
+color="black";
+width_of_line=1;
+radius=40;
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+    color=document.getElementById("color").value;
+    width_of_line=document.getElementById("width_of_line").value;
+    radius=document.getElementById("radius").value;
+    mouseEvent="mouseDown";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e){
+    mouseEvent="mouseUp";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave(e){
+    mouseEvent="mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+function mousemove(e){
+    current_position_of_mouse_x=e.clientX-canvas.offsetLeft;
+    current_position_of_mouse_y=e.clientY-canvas.offsetTop;
+
+    if(mouseEvent == "mouseDown"){
+    console.log("Current position of X and Current position of Y =");
+    console.log("X =" + current_position_of_mouse_x + "Y =" + current_position_of_mouse_y);
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=width_of_line;
+    ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2*Math.PI);
+    ctx.stroke();
+    
+    }
+}
